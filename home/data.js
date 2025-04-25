afbe5d77207ae8b69f13ec70112b54a544b654c6
sssh-jessica <jessicahsieh22@gmail.com>
2025-04-25 10:44:54 +0800

    1

---
+++ home/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "門口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9942905114406937,
+        "pitch": -0.07632783023565892,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.7572656001108022,
+          "pitch": -0.3428391494296523,
+          "title": "門口",
+          "text": "臺北市立動物園展示館相當多元化，分為戶外展示區及室內展示館兩部份，戶外展示區規劃區包括台灣鄉土動物區、兒童動物園、亞洲熱帶雨林區、沙漠動物區、澳洲動物區、非洲動物區、溫帶動物區、鳥園區、水鳥觀察區、濕地生態園、蟲蟲探索谷以及蕨園；室內展示館則有教育中心、昆蟲館、兩棲爬蟲動物館、無尾熊館、企鵝館以及酷cool節能屋，園區的多元化是您來台北市立動物園旅遊不可錯過的。"
+        }
+      ]
+    }
+  ],
+  "name": "台北市立動物園",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
